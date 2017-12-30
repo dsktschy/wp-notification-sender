718426364c0a63a4dd3b11b00d9a3af594487a66
dai <chamestica@gmail.com>
2017-12-30 17:43:02 +0900

    Add comments as WordPress plugin

--- wp-ping-expander/index.php
+++ wp-ping-expander/index.php
@@ -1,4 +1,14 @@
 <?php
+/*
+Plugin Name: WP Ping Expander
+Plugin URI: https://github.com/dsktschy/wp-ping-expander
+Description: WP Ping Expander extends the ping function built in WordPress and sends pings also when saving the pages and trashing posts and pages.
+Version: 1.0.0
+Author: dsktschy
+Author URI: https://github.com/dsktschy
+License: GPL2
+*/
+
 // Send pings also when saving pages
 add_filter('publish_page', ['WpPingExpander', 'doPings']);
 
